6bc82314029f29de082fbe4f1f0fdc2b5b88cdc0
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-03-01 17:15:49 +0100

    add aks_deployment with resource group deployment

---
+++ aks_deployment.sh
@@ -0,0 +1,11 @@
+az group create --name rg-aks-dev --location westeurope
+
+az aks create \
+    --resource-group rg-aks-dev \
+    --name aks-dev-01 \
+    --enable-pod-identity \
+    --network-plugin azure \
+    --generate-ssh-keys
+
+# credentials
+# az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
