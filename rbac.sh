0027a32b06f21d37606a53ac5df7f57faa29c350
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-03-01 19:52:17 +0100

    add rbac logic in separate file

---
+++ rbac.sh
@@ -0,0 +1,8 @@
+# Obtain the name of the resource group containing the Virtual Machine Scale set of your AKS cluster, commonly called the node resource group
+NODE_GROUP=$(az aks show --resource-group rg-aks-dev --name aks-dev-01 --query nodeResourceGroup -o tsv)
+
+# Obtain the id of the node resource group 
+NODES_RESOURCE_ID=$(az group show --name $NODE_GROUP -o tsv --query "id")
+
+# Create a role assignment granting your managed identity permissions on the node resource group
+az role assignment create --role "Virtual Machine Contributor" --assignee "$IDENTITY_CLIENT_ID" --scope $NODES_RESOURCE_ID
