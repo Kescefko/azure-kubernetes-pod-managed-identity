6abcb25743b943f97cdb9d617b3af49771638b39
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-03-01 19:55:56 +0100

    export client id of managed identity

--- rbac.sh
+++ rbac.sh
@@ -4,5 +4,7 @@ NODE_GROUP=$(az aks show --resource-group rg-aks-dev --name aks-dev-01 --query n
 # Obtain the id of the node resource group 
 NODES_RESOURCE_ID=$(az group show --name $NODE_GROUP -o tsv --query "id")
 
+export IDENTITY_CLIENT_ID="$(az identity show --resource-group ${IDENTITY_RESOURCE_GROUP} --name ${IDENTITY_NAME} --query clientId -o tsv)"
+
 # Create a role assignment granting your managed identity permissions on the node resource group
 az role assignment create --role "Virtual Machine Contributor" --assignee "$IDENTITY_CLIENT_ID" --scope $NODES_RESOURCE_ID
