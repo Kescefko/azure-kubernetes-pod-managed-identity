3421034f3a48418d8a6ae6903487ec75957a1a25
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-03-01 17:46:50 +0100

    add rbac for managed identity to control plane vmss

--- managed_identity.sh
+++ managed_identity.sh
@@ -4,3 +4,13 @@ export IDENTITY_NAME="application-identity-weu"
 az identity create --resource-group ${IDENTITY_RESOURCE_GROUP} --name ${IDENTITY_NAME}
 export IDENTITY_CLIENT_ID="$(az identity show --resource-group ${IDENTITY_RESOURCE_GROUP} --name ${IDENTITY_NAME} --query clientId -o tsv)"
 export IDENTITY_RESOURCE_ID="$(az identity show --resource-group ${IDENTITY_RESOURCE_GROUP} --name ${IDENTITY_NAME} --query id -o tsv)"
+
+
+# Obtain the name of the resource group containing the Virtual Machine Scale set of your AKS cluster, commonly called the node resource group
+NODE_GROUP=$(az aks show --resource-group rg-aks-dev --name aks-dev-01 --query nodeResourceGroup -o tsv)
+
+# Obtain the id of the node resource group 
+NODES_RESOURCE_ID=$(az group show --name $NODE_GROUP -o tsv --query "id")
+
+# Create a role assignment granting your managed identity permissions on the node resource group
+az role assignment create --role "Virtual Machine Contributor" --assignee "$IDENTITY_CLIENT_ID" --scope $NODES_RESOURCE_ID
