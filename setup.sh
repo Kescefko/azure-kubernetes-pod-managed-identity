5cac46faf3f82ffa8943f8a53a902543bd853c5b
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-03-01 17:08:02 +0100

    update setup with aks-preview extension and containerservice provider

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
 # set up
+
+# extend az cli
+az extension add --name aks-preview
+az extension update --name aks-preview
+
+# register feature and provider
+az feature register --namespace "Microsoft.ContainerService" --name "EnablePodIdentityPreview"
+az feature show --namespace "Microsoft.ContainerService" --name "EnablePodIdentityPreview"
+az provider register --namespace Microsoft.ContainerService
